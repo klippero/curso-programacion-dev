94b932435fa69842c5dbd6dae2a061dac3de35e5
Santi <s@alvarezrojo.es>
2024-11-08 23:05:44 +0100

    8

---
+++ POO/retos/estadistica/27est-moda2/py/test.py
@@ -0,0 +1,10 @@
+from sol import *
+
+lista = ListaEnteros( [8,14,9,12,14,12,15,11,12,8,10,14,12,9,12] )
+print(lista.moda())
+
+print(ListaEnteros([2,2,3,5,5,6,8,9]).moda())
+
+print(ListaEnteros([1,2,3,4,5,6,8,9]).moda())
+
+print(ListaEnteros([2,2,2,4,5,6,8,9,11,11,11,11,3,5,2,6,7,8,9,7,4,9,9]).moda())
