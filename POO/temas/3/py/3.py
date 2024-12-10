922cbb57facdcdf91b412b80689781f5a51bddf9
Santiago Álvarez <s@alvarezrojo.es>
2024-12-10 19:53:04 +0100

    10

---
+++ POO/temas/3/py/3.py
@@ -0,0 +1,15 @@
+class Frigo:
+    def __init__(self):
+        print( "se está creando un frigo")
+
+    def abre(self):
+        print( "se está abriendo" )
+
+    def cierra(self):
+        print( "se está cerrando" )
+
+    def fija_temperatura(self):
+        print( "se cambia la temperatura" )
+
+
+mi_frigo = Frigo()
---
+++ POO/temas/3/rb/3.rb
@@ -0,0 +1,20 @@
+class Frigo
+    def initialize
+        puts "se está creando un frigo"
+    end
+
+    def abre
+        puts "se está abriendo"
+    end
+
+    def cierra
+        puts "se está cerrando"
+    end
+
+    def fija_temperatura
+        puts "se cambia la temperatura"
+    end
+end
+
+
+mi_frigo = Frigo.new
