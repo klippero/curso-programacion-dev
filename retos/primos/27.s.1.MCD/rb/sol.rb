8f5bfa0032dede91d9d3c64dafcc3ad0c213e3e0
Santi <s@alvarezrojo.es>
2025-02-11 06:37:43 +0100

    11

--- retos/primos/27.s.1.MCD/rb/sol.rb
+++ retos/primos/27.s.1.MCD/rb/sol.rb
@@ -114,23 +114,3 @@ class ListaEnteros
         return result.n
     end
 end
-
-
-puts ListaEnteros.new([1500,18,6048]).MCD
-puts ListaEnteros.new([8,12,20]).MCD
-puts ListaEnteros.new([300,33880]).MCD
-puts ListaEnteros.new([180,1032]).MCD
-puts ListaEnteros.new([14,36,12]).MCD
-puts ListaEnteros.new([16,21]).MCD
-puts ListaEnteros.new([6,9]).MCD
-puts ListaEnteros.new([18,22]).MCD
-puts ListaEnteros.new([24,18]).MCD
-puts ListaEnteros.new([12,18]).MCD
-puts ListaEnteros.new([8,12,20]).MCD
-puts ListaEnteros.new([54,108,162]).MCD
-puts ListaEnteros.new([5,7,11]).MCD
-puts ListaEnteros.new([12,8]).MCD
-puts ListaEnteros.new([10,27,32]).MCD
-puts ListaEnteros.new([3,7,9]).MCD
-puts ListaEnteros.new([11,9,2,6]).MCD
-puts ListaEnteros.new([-20,25]).MCD
