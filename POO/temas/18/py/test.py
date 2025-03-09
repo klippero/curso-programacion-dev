b86af9d36d043beab69007bcf56fd04b38cee44e
Santiago Álvarez <s@alvarezrojo.es>
2025-03-09 21:19:15 +0100

    9

--- POO/temas/18/py/test.py
+++ POO/temas/18/py/test.py
@@ -2,14 +2,14 @@ yo = 10
 luis = 17
 antonio = 21
 
-# Y
+# Y and
 if yo < 18 and luis < 18:
     print("Luis y yo somos menores de edad")
 
-# O
+# O or
 if yo > 18 or antonio > 18:
     print("Antonio o yo somos mayores de edad")
 
-# NO
+# NO not
 if not (yo > 18):
     print("Yo no soy mayor de edad")
--- POO/temas/18/rb/test.rb
+++ POO/temas/18/rb/test.rb
@@ -2,17 +2,17 @@ yo = 10
 luis = 17
 antonio = 21
 
-# Y
+# Y &&
 if yo < 18 && luis < 18
     puts "Luis y yo somos menores de edad"
 end
 
-# O
+# O ||
 if yo > 18 || antonio > 18
     puts "Antonio o yo somos mayores de edad"
 end
 
-# NO
+# NO !
 if ! (yo > 18)
     puts "Yo no soy mayor de edad"
 end
