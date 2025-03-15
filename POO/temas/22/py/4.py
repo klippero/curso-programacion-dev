4a82aefc5f38de92c0d35725e13d5888847c6fd3
Santi <s@alvarezrojo.es>
2025-03-15 11:07:04 +0100

    15

---
+++ POO/temas/22/py/4.py
@@ -0,0 +1,4 @@
+nums = [12,34,56,43,2,7,34,100]
+
+for i in range(1,len(nums)-1):
+    print( f"({i}) {nums[i]} ",end="")
---
+++ POO/temas/22/rb/4.rb
@@ -0,0 +1,5 @@
+nums = [12,34,56,43,2,7,34,100]
+
+(1..nums.length-2).each do |i|
+    print "(#{i}) #{nums[i]} "
+end
