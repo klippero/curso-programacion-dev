a525f325dc74e65d37165b62f58ae11cf1119c39
Santiago Álvarez <s@alvarezrojo.es>
2025-03-21 22:06:51 +0100

    21

--- working/ejercicios/sopaletras/s.rb
+++ working/ejercicios/sopaletras/s.rb
@@ -1,22 +1,42 @@
 class Sopa
-    ANCHO = 7
-
-    def initialize
+    def initialize(ancho,alto)
         @sopa = []
-        ANCHO.times do
+        alto.times do
             linea = []
-            ANCHO.times do
-                linea << ' '    # rand(65..90).chr
+            ancho.times do
+                linea << ' '
             end
             @sopa << linea
         end
     end
 
-    def to_s
+    def ancho
+        return @sopa[0].length
+    end
+
+    def alto
+        return @sopa.length
+    end
+
+    def letter(coor)
+        return @sopa[coor[0],coor[1]]
+    end
+
+    def set(coor,letter)
+        @sopa[coor[0]][coor[1]] = letter
+    end
+
+    def draw(blank=false)
         result = ""
         @sopa.each do |linea|
-            linea.each do |word|
-                result << word
+            linea.each do |letter|
+                if letter != ' '
+                    result << letter
+                elsif blank
+                    result << ' '
+                else
+                    result << rand(65..90).chr
+                end
                 result << " "
             end
             result << "\n"
@@ -24,55 +44,77 @@ class Sopa
         return result
     end
 
+    def to_s
+        return draw
+    end
+
     def en_sopa?(coor)
-        return coor>=0 && coor<ANCHO
+        return coor[0]>0 && coor[0]<ancho && coor[1]>0 && coor[1]<alto
     end
 
     def <<(word)
-        inicio = [ rand(ANCHO), rand(ANCHO) ]
-        orientacion = [ rand(-1..1), rand(-1..1) ]
-        while orientacion == [0,0]
-            orientacion = [ rand(-1..1), rand(-1..1) ]
-        end
-        fin = [ inicio[0] + (word.length-1) * orientacion[0], inicio[1] + (word.length-1) * orientacion[1] ]
-        while ! ( en_sopa?(fin[0]) && en_sopa?(fin[1]) )
-            inicio = [ rand(ANCHO), rand(ANCHO) ]
-            orientacion = [ rand(-1..1), rand(-1..1) ]
-            while orientacion == [0,0]
-                orientacion = [ rand(-1..1), rand(-1..1) ]
-            end
-            fin = [ inicio[0] + (word.length-1) * orientacion[0], inicio[1] + (word.length-1) * orientacion[1] ]
-        end
-        puts 1
+        w = Word.new(self,word)
     end
 end
 
 class Word
-    def initialize(sopa)
+    def initialize(sopa,word)
         @sopa = sopa
+        @word = word
+        posiciona
     end
 
     def intenta_posicionar
-        @inicio = [ rand(sopa.ANCHO), rand(sopa.ANCHO) ]
-        @orientacion = [ rand(-1..1), rand(-1..1) ]
+        @inicio = [ rand(@sopa.ancho), rand(@sopa.alto) ]
+        @orientacion = [ rand(-1..1), rand(0..1) ]
         while @orientacion == [0,0]
             @orientacion = [ rand(-1..1), rand(-1..1) ]
         end
-        @fin = [ inicio[0] + (word.length-1) * orientacion[0], inicio[1] + (word.length-1) * orientacion[1] ]
+        @fin = [ @inicio[0] + (@word.length-1) * @orientacion[0], @inicio[1] + (@word.length-1) * @orientacion[1] ]
     end
 
-    def en_sopa?
-        return 
+    def possible?
+        coor = [@inicio[0],@inicio[1]]
+        i = 0
+        posible = true
+        while coor != @fin && posible
+            if [' ',@word[i]].include?(@sopa.letter(coor))
+                posible = false
+            elsif coor[0] == @sopa.ancho || coor[1] == @sopa.alto
+                posible = false
+            else
+                i = i + 1
+                coor[0] = coor[0] + (1*@orientacion[0])
+                coor[1] = coor[1] + (1*@orientacion[1])
+            end
+        end
+        return posible
+    end
 
     def posiciona
         intenta_posicionar
-        while ! ( sopa.en_sopa?(@fin[0]) && sopa.en_sopa?(@fin[1]) )
+        while ! possible?
             intenta_posicionar
         end
+        set
+    end
+
+    def set
+        coor = [@inicio[0],@inicio[1]]
+        i = 0
+        while coor != @fin
+            @sopa.set(coor,@word[i])
+            i = i + 1
+            coor[0] = coor[0] + (1*@orientacion[0])
+            coor[1] = coor[1] + (1*@orientacion[1])
+        end
+        @sopa.set(coor,@word[i])
     end
 end
 
 
-s = Sopa.new
+s = Sopa.new(7,5)
 puts s
+puts
 s << "CASA"
+puts s.draw(true)
