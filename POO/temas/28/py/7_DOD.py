03f21ef67dd10f41ea1ec8b001414bb9146087cc
Santi <s@alvarezrojo.es>
2025-03-22 12:55:22 +0100

    22

--- POO/temas/28/py/7_DOD.py
+++ POO/temas/28/py/7_DOD.py
@@ -1,8 +1,7 @@
 # opcional no puede preceder
 # a uno por defecto
 
-def sponge(a=7,*o,b=9)
-    puts "Valores: #{a}, #{o}, #{b}"
-end
+def sponge(a=7,*o,b=9):
+    print(f"Valores: {a}, {o}, {b}")
 
 sponge(1,2)
---
+++ POO/temas/28/py/8.py
@@ -0,0 +1,4 @@
+def example_method(*args):
+    print(f"Positional arguments: {args}")
+
+example_method(1, 2, 3)
